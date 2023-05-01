63a0f8256643574250001b915821c470c587c482
Brady Boettcher <bradyboettcher@gmail.com>
2023-05-01 10:55:39 -0700

    key component graphics

--- Source/Components/KeyboardComponent.cpp
+++ Source/Components/KeyboardComponent.cpp
@@ -2,16 +2,20 @@
 
 //==============================================================================
 KeyboardComponent::KeyboardComponent() {
-  for (int i = 0; i < Utils::NUM_DEFAULT_PITCH_KEYS; ++i) {
-    mKeys.add(Utils::Key(Utils::DEFAULT_PITCH_KEYS[i].keyCode));
-  }
 }
 
 KeyboardComponent::~KeyboardComponent() {}
 
 //==============================================================================
 void KeyboardComponent::paint(juce::Graphics& g) {
-
+  // Draw key rects
+  for (Key& key : mKeys) {
+    juce::Colour col = key.isPressed ? juce::Colours::lightgrey : juce::Colours::white;
+    g.setColour(col);
+    g.fillRoundedRectangle(key.rect.toFloat(), Utils::ROUNDED_AMOUNT);
+    g.setColour(juce::Colours::black);
+    g.drawFittedText(juce::String(&key.keyChar, 1), key.rect, juce::Justification::centred, 1);
+  }
 
 #ifdef OVERLAY_LABELS
   g.fillAll(juce::Colours::dimgrey);
@@ -24,14 +28,26 @@ void KeyboardComponent::paint(juce::Graphics& g) {
 }
 
 void KeyboardComponent::resized() {
-
+  juce::Rectangle<int> r = getLocalBounds();
+  r.reduce(Utils::PADDING, Utils::PADDING);
+  const float keySize = (r.getWidth() - (Utils::PADDING * (NUM_KEYS_ROW_1 - 1))) / NUM_KEYS_ROW_1;
+  juce::Rectangle<int> rows[3] = {r.removeFromTop(keySize + Utils::PADDING).withTrimmedBottom(Utils::PADDING),
+                                  r.removeFromTop(keySize + Utils::PADDING).withTrimmedBottom(Utils::PADDING), r};
+  rows[1] = rows[1].withTrimmedLeft(keySize / 4.0f);
+  for (int i = 0; i < mKeys.size(); ++i) {
+    int row = (i >= NUM_KEYS_ROW_1);
+    if (i >= NUM_PITCH_KEYS) row = 2;
+    mKeys[i].rect = rows[row].removeFromLeft(keySize);
+    rows[row].removeFromLeft(Utils::PADDING);
+  }
 }
 
-bool KeyboardComponent::keyPressed(const juce::KeyPress& keyPress, juce::Component* originatingComponent) {
-  int code = keyPress.getKeyCode();
-  auto key = std::find_if(mKeys.begin(), mKeys.end(), [code](const Utils::Key& key) { return key.keyCode == code; });
-  if (key != mKeys.end()) {
-    key->isPressed = keyPress.isCurrentlyDown();
+bool KeyboardComponent::keyPressed(const juce::KeyPress& key, juce::Component* originatingComponent) { return false; }
+
+bool KeyboardComponent::keyStateChanged(bool isKeyDown, juce::Component* originatingComponent) {
+  for (Key& key : mKeys) {
+    key.isPressed = juce::KeyPress::isKeyCurrentlyDown(key.keyCode);
   }
-  return true; 
+  repaint();
+  return true;
 }
--- Source/Components/KeyboardComponent.h
+++ Source/Components/KeyboardComponent.h
@@ -14,12 +14,50 @@ class KeyboardComponent : public juce::Component, public juce::KeyListener {
   void resized() override;
 
   bool keyPressed(const juce::KeyPress &key, juce::Component *originatingComponent) override;
+  bool keyStateChanged(bool isKeyDown, juce::Component *originatingComponent) override;
 
  private:
-  static constexpr float KEY_SIZE = 1.0f / 10.0f;
   static constexpr float COL_STAGGER = 0.25f;
+  static constexpr int NUM_KEYS_ROW_1 = 10; // Used for drawing keys
+  static constexpr int NUM_PITCH_KEYS = 19;
+  static constexpr int NUM_ROWS = 3;
+  static constexpr int C1 = 24;
+  static constexpr int NUM_DEFAULT_KEYS = 19;
 
-  juce::Array<Utils::Key> mKeys;
+  enum KeyType { PITCH, UNMAPPED };
+
+  typedef struct Key {
+    Key(char keyChar, int keyCode) : type(UNMAPPED), keyChar(keyChar), keyCode(keyCode) {}
+    Key(char keyChar, int keyCode, int midiNote) : type(PITCH), keyChar(keyChar), keyCode(keyCode), midiNote(midiNote) {}
+    bool isPressed = false;
+    KeyType type;
+    char keyChar;
+    int keyCode;
+    int midiNote = Utils::PitchClass::NONE;  // Should be relative to C1, only used for PITCH type Keys
+    juce::Rectangle<int> rect;
+  } Key;
+
+  std::array<Key, NUM_DEFAULT_KEYS> mKeys = {
+      Key('Q', 81),                                                    // Q
+      Key('W', 87, Utils::PitchClass::Cs),                             // W
+      Key('E', 69, Utils::PitchClass::Ds),                             // E
+      Key('R', 82),                                                    // R
+      Key('T', 84, Utils::PitchClass::Fs),                             // T
+      Key('Y', 89, Utils::PitchClass::Gs),                             // Y
+      Key('U', 85, Utils::PitchClass::As),                             // U
+      Key('I', 73),                                                    // I
+      Key('O', 79, Utils::PitchClass::Cs + Utils::PitchClass::COUNT),  // O
+      Key('P', 80),                                                    // P
+      Key('A', 65, Utils::PitchClass::C),                              // A
+      Key('S', 83, Utils::PitchClass::D),                              // S
+      Key('D', 68, Utils::PitchClass::E),                              // D
+      Key('F', 70, Utils::PitchClass::F),                              // F
+      Key('G', 71, Utils::PitchClass::G),                              // G
+      Key('H', 72, Utils::PitchClass::A),                              // H
+      Key('J', 74, Utils::PitchClass::B),                              // J
+      Key('K', 75, Utils::PitchClass::C + Utils::PitchClass::COUNT),   // K
+      Key('L', 76, Utils::PitchClass::D + Utils::PitchClass::COUNT)    // L
+  };
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(KeyboardComponent)
 };
